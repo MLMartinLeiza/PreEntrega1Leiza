f693972393bbbcaaa5d796d978d7d17d150fc77c
MLMartinLeiza <martin.leiza@gmail.com>
2024-04-18 14:26:16 -0300

    Se quitaron algunos alert y prompt

--- js/main.js
+++ js/main.js
@@ -7,122 +7,135 @@ Usuario.prototype.calcularFechaNacimiento = function () {
     const fechaActual = new Date();
     const anioActual = fechaActual.getFullYear();
     const anioNacimiento = anioActual - this.edad;
-    alert(`Registrado correctamente. ${this.nombre} naciste aproximadamente en ${anioNacimiento}.`);
+    return `Registrado correctamente. ${this.nombre} naciste aproximadamente en ${anioNacimiento}.`;
 };
 
-let juan = new Usuario("Juan", 30);
-let german = new Usuario("German", 30);
-let martin = new Usuario("Martin", 29);
+const usuariosPreestablecidos = {
+    juan: new Usuario("Juan", 30),
+    german: new Usuario("German", 30),
+    martin: new Usuario("Martin", 29)
+};
 
 function verificarUsuario(nombre) {
-    return nombre === juan.nombre || nombre === german.nombre || nombre === martin.nombre;
+    const nombreLowerCase = nombre.toLowerCase();
+
+    return Object.keys(usuariosPreestablecidos).some(key => key.toLowerCase() === nombreLowerCase);
 }
+
+const sesionForm = document.getElementById('sesion-form');
+const registroForm = document.getElementById('registro');
+const nombreRegistroInput = document.getElementById('nombreRegistro');
+const edadRegistroInput = document.getElementById('edadRegistro');
+const consultas = document.getElementById("consultas");
 
 function gestionarSesion() {
-    let nombreUsuario = prompt("Ingresa tu nombre");
-    if (verificarUsuario(nombreUsuario)) {
-        alert(`¡Bienvenido de nuevo, ${nombreUsuario}!`);
-        return;
-    }
+    const nombre = sesionForm.elements['nombre'].value;
+    const edad = parseInt(sesionForm.elements['edad'].value);
 
-    alert("No estas registrado, ingresa tus datos a continuación");
-    let nombre = solicitarNombre();
-    let edad = solicitarEdad();
-    let nuevoUsuario = new Usuario(nombre, edad);
-    nuevoUsuario.calcularFechaNacimiento();
+    if (verificarUsuario(nombre)) {
+        alert(`¡Bienvenido de nuevo, ${nombre}!`);
+    } else {
+        if (edad < 18) {
+            alert("Acceso denegado");
+            return;
+        }
+        mostrarFormularioRegistro();
+    }
 }
 
-gestionarSesion();
-
-function solicitarNombre() {
-    let nombre = prompt("Ingresa tu nombre");
-    while (nombre === "") {
-        nombre = prompt("Ingresa tu nombre");
-    }
-    console.log(nombre);
-    alert("Hola " + nombre);
-    return nombre;
+function mostrarFormularioRegistro() {
+    document.getElementById('sesion').style.display = 'none';
+    document.getElementById('registro').style.display = 'block';
 }
 
-function solicitarEdad() {
-    let edad = prompt("Ingresa tu edad");
-    while (isNaN(edad) || edad < 18) {
-        alert("Acceso denegado");
-        edad = prompt("Ingresa tu edad");
-    }
-    alert("Acceso permitido");
-    return Number(edad);
+function registrarse() {
+    const nombre = nombreRegistroInput.value;
+    const edad = parseInt(edadRegistroInput.value);
+    alert(new Usuario(nombre, edad).calcularFechaNacimiento());
 }
 
-function solicitarNumero(mensaje) {
-    let numero = prompt(mensaje);
-    while (isNaN(numero) || numero <= 0) {
-        alert("Ingrese un número válido.");
-        numero = prompt(mensaje);
+function calcularPrestamo() {
+    const montoPrestamo = parseInt(document.getElementById('montoPrestamo').value);
+    const plazoPrestamo = parseInt(document.getElementById('plazoPrestamo').value);
+    const tasaInteresAnual = 180;
+
+    if (montoPrestamo <= 0 || tasaInteresAnual <= 0 || plazoPrestamo <= 0) {
+        alert("Por favor, ingrese valores válidos.");
+    } else {
+        const pagoMensual = calcularPagoMensual(montoPrestamo, tasaInteresAnual, plazoPrestamo);
+        document.getElementById('resultado-prestamo').innerText = `El pago mensual será de $${pagoMensual.toFixed(2)}. ${plazoPrestamo} cuotas.`;
     }
-    return numero;
 }
 
 function calcularPagoMensual(montoPrestamo, tasaInteresAnual, plazoEnMeses) {
-    const capitalTotal = montoPrestamo * tasaInteresAnual
-    const pagoMensual = (capitalTotal / plazoEnMeses);
+    const capitalTotal = montoPrestamo * tasaInteresAnual;
+    const pagoMensual = capitalTotal / plazoEnMeses;
     return pagoMensual;
 }
 
-let montoPrestamo = solicitarNumero("Ingrese el monto del préstamo:");
-let plazoEnMeses = solicitarNumero("Ingrese el plazo del préstamo en meses:");
+// Menu de opciones con DOM y eventos
 
-const tasaInteresAnual = 180 / 100;
+document.addEventListener('DOMContentLoaded', function () {
 
-if (montoPrestamo <= 0 || tasaInteresAnual <= 0 || plazoEnMeses <= 0) {
-    alert("Por favor, ingrese valores válidos.");
-} else {
+    const botonContinuar = document.getElementById('botonContinuar');
 
-    const pagoMensual = calcularPagoMensual(montoPrestamo, tasaInteresAnual, plazoEnMeses);
-    console.log(`El pago mensual será de $${pagoMensual.toFixed(2)}. ` + plazoEnMeses + " cuotas.");
-    alert(`El pago mensual será de $${pagoMensual.toFixed(2)}. ` + plazoEnMeses + " cuotas.");
-}
+    botonContinuar.addEventListener('click', function () {
+        const select = document.getElementById('opciones');
+        const opcionSeleccionada = select.value;
 
-const masConsultas = ["1. Búsqueda", "2. Ordenar por tasa de interés ", "3. Consultar Tasa de Interés ",];
-let opcionSeleccionada = prompt("Seleccione una opción:\n" + masConsultas.join("\n"));
-while (opcionSeleccionada !== "1" && opcionSeleccionada !== "2" && opcionSeleccionada !== "3") {
-    opcionSeleccionada = prompt("Opción mo válida. Seleccione una opción válida:\n" + masConsultas.join("\n"));
-}
+        switch (opcionSeleccionada) {
+            case 'opcion1':
+                const prestamoSolicitado = document.getElementById('prestamo-solicitado');
+                prestamoSolicitado.innerText = ("Prestamo solicitado correctamente");
 
-console.log("Opción seleccionada: " + masConsultas[opcionSeleccionada - 1]);
-alert("Ha seleccionado: " + masConsultas[opcionSeleccionada - 1]);
+                break;
+            case 'opcion2':
+                const formularioBusqueda = document.getElementById('busqueda');
+                formularioBusqueda.classList.remove('d-none');
 
-const palabrasClavePrestamo = ["prestamo hipotecario", "prestamo personal", "prestamo para empresas", "prestamo en dolares"];
+                const buscarButton = document.getElementById('buscar');
 
-if (opcionSeleccionada === "1") {
-    let entradaUsuario = prompt("Ingrese su consulta:").toLowerCase().trim();
-    let palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario));
+                buscarButton.addEventListener('click', function () {
+                    const palabrasClavePrestamo = ["préstamo hipotecario", "préstamo personal", "préstamo para empresas", "préstamo en dólares"];
+                    const entradaUsuario = document.getElementById('palabra-busqueda').value.toLowerCase().trim();
+                    let palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario));
 
-    if (palabrasEncontradas.length === 0) {
-        palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario) || palabra.toLowerCase().includes("pres"));
-    }
+                    if (palabrasEncontradas.length === 0) {
+                        palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario) || palabra.toLowerCase().includes('prestamo'));
+                    }
 
-    if (palabrasEncontradas.length > 0) {
-        let listaPalabras = palabrasEncontradas.map(palabra => `- ${palabra}`).join("\n");
-        alert("Palabras clave relacionadas con 'préstamo':\n" + listaPalabras);
-    } else {
-        alert("No se encontraron palabras clave relacionadas con 'préstamo'.");
-    }
-}
+                    const resultadoBusqueda = document.getElementById('resultado-busqueda');
 
-const prestamos = [
-    { tipo: "préstamo hipotecario", tasaInteres: 0.05 },
-    { tipo: "préstamo personal", tasaInteres: 0.08 },
-    { tipo: "préstamo para empresas", tasaInteres: 0.1 },
-    { tipo: "préstamo en dólares", tasaInteres: 0.06 }
-];
+                    if (palabrasEncontradas.length > 0) {
+                        let listaPalabras = palabrasEncontradas.map(palabra => `- ${palabra}`).join("\n");
+                        resultadoBusqueda.innerText = "Palabras clave relacionadas con 'préstamo':\n" + listaPalabras;
+                    } else {
+                        resultadoBusqueda.innerText = "No se encontraron palabras clave relacionadas con 'préstamo'.";
+                    }
+                });
 
-if (opcionSeleccionada === "2") {
-    prestamos.sort((a, b) => a.tasaInteres - b.tasaInteres);
+                break;
 
-    let listaPrestamos = prestamos.map(prestamo => `- ${prestamo.tipo}: ${prestamo.tasaInteres * 100}%`).join("\n");
+            case 'opcion3':
+                const prestamos = [
+                    { tipo: "préstamo hipotecario", tasaInteres: 0.05 },
+                    { tipo: "préstamo personal", tasaInteres: 0.08 },
+                    { tipo: "préstamo para empresas", tasaInteres: 0.1 },
+                    { tipo: "préstamo en dólares", tasaInteres: 0.06 }
+                ];
 
-    alert("Prestamos ordenados por tasa de interés (de menor a mayor):\n" + listaPrestamos);
-}
+                if (opcionSeleccionada === "2") {
+                    prestamos.sort((a, b) => a.tasaInteres - b.tasaInteres);
 
+                    let listaPrestamos = prestamos.map(prestamo => `- ${prestamo.tipo}: ${prestamo.tasaInteres * 100}%`).join("\n");
 
+                    alert("Prestamos ordenados por tasa de interés (de menor a mayor):\n" + listaPrestamos);
+                }
+
+                break;
+
+            default:
+                console.log('Opción no válida');
+        }
+    });
+});
