91fa8fb1e5bc43c2ba66e5e8d00d77ce5dd32464
MLMartinLeiza <martin.leiza@gmail.com>
2024-04-20 17:47:35 -0300

    Correcciones generales

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ const usuariosPreestablecidos = {
 
 function verificarUsuario(nombre) {
     const nombreLowerCase = nombre.toLowerCase();
-
     return Object.keys(usuariosPreestablecidos).some(key => key.toLowerCase() === nombreLowerCase);
 }
 
@@ -73,49 +72,32 @@ function calcularPagoMensual(montoPrestamo, tasaInteresAnual, plazoEnMeses) {
     return pagoMensual;
 }
 
-// Menu de opciones con DOM y eventos
-
 document.addEventListener('DOMContentLoaded', function () {
-
     const botonContinuar = document.getElementById('botonContinuar');
-
     botonContinuar.addEventListener('click', function () {
         const select = document.getElementById('opciones');
         const opcionSeleccionada = select.value;
 
         switch (opcionSeleccionada) {
             case 'opcion1':
-                const prestamoSolicitado = document.getElementById('prestamo-solicitado');
-                prestamoSolicitado.innerText = ("Prestamo solicitado correctamente");
-
+                document.getElementById('prestamo-solicitado').innerText = "Préstamo solicitado correctamente";
                 break;
             case 'opcion2':
                 const formularioBusqueda = document.getElementById('busqueda');
                 formularioBusqueda.classList.remove('d-none');
 
                 const buscarButton = document.getElementById('buscar');
-
                 buscarButton.addEventListener('click', function () {
                     const palabrasClavePrestamo = ["préstamo hipotecario", "préstamo personal", "préstamo para empresas", "préstamo en dólares"];
                     const entradaUsuario = document.getElementById('palabra-busqueda').value.toLowerCase().trim();
-                    let palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario));
-
-                    if (palabrasEncontradas.length === 0) {
-                        palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario) || palabra.toLowerCase().includes('prestamo'));
-                    }
+                    let palabrasEncontradas = palabrasClavePrestamo.filter(palabra => palabra.toLowerCase().includes(entradaUsuario) || palabra.toLowerCase().includes('préstamo'));
 
                     const resultadoBusqueda = document.getElementById('resultado-busqueda');
-
-                    if (palabrasEncontradas.length > 0) {
-                        let listaPalabras = palabrasEncontradas.map(palabra => ` ${palabra}`).join("\n");
-                        resultadoBusqueda.innerText = "Resultado de la busqueda:\n" + listaPalabras;
-                    } else {
-                        resultadoBusqueda.innerText = "No se encontraron palabras clave relacionadas con 'préstamo'.";
-                    }
+                    resultadoBusqueda.innerText = palabrasEncontradas.length > 0 ?
+                        "Resultado de la búsqueda:\n" + palabrasEncontradas.join("\n") :
+                        "No se encontraron palabras clave relacionadas con 'préstamo'.";
                 });
-
                 break;
-
             case 'opcion3':
                 const prestamos = [
                     { tipo: "préstamo hipotecario", tasaInteres: 0.05 },
@@ -123,17 +105,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     { tipo: "préstamo para empresas", tasaInteres: 0.1 },
                     { tipo: "préstamo en dólares", tasaInteres: 0.06 }
                 ];
-
-                if (opcionSeleccionada === "2") {
-                    prestamos.sort((a, b) => a.tasaInteres - b.tasaInteres);
-
-                    let listaPrestamos = prestamos.map(prestamo => `- ${prestamo.tipo}: ${prestamo.tasaInteres * 100}%`).join("\n");
-
-                    alert("Prestamos ordenados por tasa de interés (de menor a mayor):\n" + listaPrestamos);
-                }
-
+                prestamos.sort((a, b) => a.tasaInteres - b.tasaInteres);
+                alert("Préstamos ordenados por tasa de interés (de menor a mayor):\n" +
+                    prestamos.map(prestamo => `- ${prestamo.tipo}: ${prestamo.tasaInteres * 100}%`).join("\n"));
                 break;
-
             default:
                 console.log('Opción no válida');
         }
